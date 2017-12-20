2f3a230bc84dc47ec9a0360ec4d9fb159fc6d618
flowain <f.waidner@gmx.de>
2017-12-20 01:22:28 +0100

    Instantiate Firestore Controller

---
+++ src/main/java/de/addrinks/backend/firestore/DatabaseController.java
@@ -0,0 +1,18 @@
+package de.addrinks.backend.firestore;
+
+import java.io.IOException;
+
+public interface DatabaseController {
+	static final String PROJECTID = "addrinks-dev";
+	
+	static final String PRODUKTTABELLE = "produkte";
+	static final String KATEGORIETABELLE = "kategorie";
+	static final String BESTELLUNGENTABELLE = "bestellungen";
+	static final String KUNDENTABELLE = "kunden";
+	static final String ABONNEMENTTABELLE = "abonnements";
+	static final String VIDEOCONTENTTABELLE = "video_content";
+	static final String PUNKTEPROVIEWTABELLE = "punkte_pro_view";
+	static final String VIEWSTABELLE = "views";
+
+
+}
---
+++ src/main/java/de/addrinks/backend/firestore/FirestoreControllerImpl.java
@@ -0,0 +1,56 @@
+package de.addrinks.backend.firestore;
+
+import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
+public class FirestoreControllerImpl implements DatabaseController {
+	// Use the application default credentials
+	GoogleCredentials credentials;
+	FirebaseOptions options;
+	Firestore db;
+	
+	public FirestoreControllerImpl() {
+		try {
+			init();
+		} catch (IOException e) {
+			//TODO Handle Exception
+			e.printStackTrace();
+		}
+	}
+	
+	private void init() throws IOException{
+		credentials = GoogleCredentials.getApplicationDefault();
+		
+		options = new FirebaseOptions.Builder()
+			    .setCredentials(credentials)
+			    .setProjectId(PROJECTID)
+			    .build();
+		
+		FirebaseApp.initializeApp(options);
+		
+		db = FirestoreClient.getFirestore();
+	}
+	 
+	public CollectionReference getCollectionReference(String collectionName){
+		CollectionReference collection = db.collection(collectionName);
+		
+		return collection;
+	}
+	
+	public DocumentReference getDocumentReference(CollectionReference collection, String documentName){
+		DocumentReference document = collection.document(documentName);
+		return document;
+	}
+	
+	public DocumentReference getDocumentReferenceSubCollection(CollectionReference collection, String documentName, String subCollection, String subDocumentName){
+		DocumentReference subDocument = collection.document(documentName).collection(subCollection).document(subDocumentName);
+		return subDocument;
+	}
+}
